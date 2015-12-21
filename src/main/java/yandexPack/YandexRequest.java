de85efd6760c694db0fe9f69849bc0063453fbb9
TemplaRus <RusTemplaR@yandex.com>
2015-12-21 21:06:02 +0300

    а теперь разбираем то, что пришло на улицу\адрес

--- src/main/java/yandexPack/YandexRequest.java
+++ src/main/java/yandexPack/YandexRequest.java
@@ -34,6 +34,8 @@ public class YandexRequest {
     private InputStream in;
     private URLConnection conn;
     private OutputStreamWriter out;
+    private double lat;
+    private double lon;
 
     public YandexRequest() {
         sendDataURL();
@@ -125,11 +127,131 @@ public class YandexRequest {
         } catch (IOException ex) {
             Logger.getLogger(YandexRequest.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-//                JSONObject jAnsw = new JSONObject(new String(incoming));
-//                System.out.println("jAnsw=" + jAnsw);
         System.out.println("Client query(" + length + " bytes):\n" + new String(incoming).trim());
-        // закрываем файл
+        JSONObject jAnsw = new JSONObject(new String(incoming));
+        System.out.println("jAnsw=" + jAnsw);
+
+        try {
+            out.close();
+            in.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(YandexRequest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+// ----------------------------------------------------------------------------        
+//         вариант 1        
+        System.err.println(jAnsw.keySet().toString());
+        JSONObject jAnsw1 = jAnsw.getJSONObject("position");
+        System.out.println("-=-=-=-=-=-=-=-=-=-==-");
+        System.err.println(jAnsw1.keySet().toString());
+        lat = (Double) jAnsw1.getDouble("latitude");
+        lon = (Double) jAnsw1.getDouble("longitude");
+// ----------------------------------------------------------------------------
+////         вариант 2          не пашет - в keyset только position почему-то
+// ----------------------------------------------------------------------------        
+//        System.err.println(jAnsw.keySet().toString());
+//        lat = jAnsw.optDouble("latitude");
+//        lon = jAnsw.optDouble("longitude");
+
+        //https://geocode-maps.yandex.ru/1.x/?format=json&geocode=37.470653,55.664897
+        getLocation();
     }
 
+    private void getLocation() {
+        String host = "https://geocode-maps.yandex.ru/1.x/?format=json&geocode=" + lon + "," + lat + "";
+
+        try {
+            conn = new URL(host).openConnection();
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(YandexRequest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(YandexRequest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // conn.setDoOutput(false);
+        conn.setDoInput(true);
+// -----------------------------------------------------------------------------------------------        
+        //вариант номер 1
+        StringBuilder strBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                strBuilder.append(line + '\n');
+            }
+        } catch (IOException ex) {
+            System.err.println("IOEx" + ex);
+        }
+        System.out.println(strBuilder);
+        System.err.println("--=-=-=-=-=-=-=-=-=-=-=-=");
+        JSONObject jAnsw = new JSONObject(strBuilder.toString());
+
+// ----------------------------------------------------------------------------------------------        
+//        //вариант номер 2
+//         char[] incoming = new char[8192];
+//        try {
+//            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+//
+//           
+//            length = in.read(incoming);
+//        } catch (IOException ex) {
+//            Logger.getLogger(YandexRequest.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        System.out.println("Client query(" + length + " bytes):\n" + new String(incoming).trim());
+//
+//        JSONObject jAnsw = new JSONObject(new String(incoming).trim());
+        System.out.println("jAnsw=" + jAnsw.toString());
+        try {
+            out.close();
+            in.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(YandexRequest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for (String street : getAdress(jAnsw)) {
+            System.err.println("ОМГ ВАХАВАХВАХ МЫ ТАКИ НАШЛИ УЛИЦУ!!1212123124!!!212");
+            System.out.println("Street: " + street);
+        }
+    }
+
+    private String[] getAdress(JSONObject jsonObj) {
+        
+        System.out.println(jsonObj.keySet());
+        JSONObject response = jsonObj.getJSONObject("response");
+        System.out.println("response keyset=\n"+response.keySet());
+        System.err.println("--=-=-=-=-=-=-=-=-=-=-=-=");
+        
+        JSONObject GeoObjectCollection = response.getJSONObject("GeoObjectCollection");
+        System.out.println("\tGeoObjectCollection keyset=\n\t"+GeoObjectCollection.keySet());
+        System.err.println("\t--=-=-=-=-=-=-=-=-=-=-=-=\n");
+                
+        JSONObject metaDataProperty = GeoObjectCollection.getJSONObject("metaDataProperty");
+                System.out.println("\t\tmetaDataProperty keyset=\n\t\t"+metaDataProperty.keySet());
+                
+                JSONArray featureMember = GeoObjectCollection.getJSONArray("featureMember");
+                System.out.println("\t\tfeatureMember toString=\n\t\t"+featureMember.toString());
+                System.err.println("\t\t--=-=-=-=-=-=-=-=-=-=-=-=");
+                int streetNum=1;
+                for(int i=0;i<featureMember.length();i++ )
+                {
+                    System.err.println("Step="+i+" String="+featureMember.get(i).toString());
+                }
+                System.err.println("\t\t--=-=-=-=-=-=-=-=-=-=-=-=");
+//                да хрен так просто это работает..                
+                String ThoroughfareName = featureMember.optString(0);
+                System.out.println("\t\t\tThoroughfareName =\n\t\t\t"+ThoroughfareName);
+                System.err.println("\t\t\t+-+-+-++-+-+-+-+--+-+-+-+-+");
+                
+                
+                JSONObject GeoObject = featureMember.getJSONObject(0);
+                System.out.println("\tGeoObject keyset=\n\t"+GeoObject.keySet());
+                System.err.println("\t--=-=-=-=-=-=-=-=-=-=-=-=\n");        
+                
+                JSONObject Thoroughfare=GeoObject.getJSONObject("GeoObject").getJSONObject("metaDataProperty").getJSONObject("GeocoderMetaData").getJSONObject("AddressDetails").getJSONObject("Country").getJSONObject("AdministrativeArea").getJSONObject("SubAdministrativeArea").getJSONObject("Locality").getJSONObject("Thoroughfare");
+              System.err.println("  Thoroughfare.getString(ThoroughfareName)="+  Thoroughfare.getString("ThoroughfareName"));
+                
+        String[] streets=new String[streetNum];
+        streets[0]=  Thoroughfare.getString("ThoroughfareName");
+        return streets;
+    }
 }
